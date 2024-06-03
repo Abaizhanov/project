7ba8f16c6cfb26aefb37566999e1fd1b5c903dba
Temirlan <example@gmail.com>
2024-06-04 01:12:38 +0600

    update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 "use strict"
 //alert('Hello');
 //const result = confirm("Are you here?");
-const answer = +prompt("How old are you?", "5");
+const answer = prompt("How old are you?", "5");
 console.log(typeof(answer));
