bfa9c5dbcb7192dc0d076d08b6fabad8eae7adfb
Temirlan <example@gmail.com>
2024-06-04 22:48:33 +0600

    lesson 18

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 "use strict"
 //alert('Hello');
 //const result = confirm("Are you here?");
-const answer = prompt("How old are you?", "5");
-console.log(typeof(answer));
+//const answer = prompt("How old are you?", "5");
+//console.log(typeof(answer));
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+    actors: {},
+    count: numberOfFilms,
+    genres: [],
+    movies: {},
+    privat: false
+};
+
+const a= prompt("Один из последниз просмотренных фильмов?", ""),
+      b= prompt("Оцените его", ""),
+      c= prompt("Один из последниз просмотренных фильмов?", ""),
+      d= prompt("Оцените его", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB)
